7b1f3a903df463b8edc7c15021799feeca2a1917
Muhammad Hasannudin Yusa <emhayusa@gmail.com>
2021-08-06 12:01:39 +0700

    fixing edit and update data

--- src/Data.js
+++ src/Data.js
@@ -18,13 +18,14 @@ export default function Data(props) {
     //const password = '******';
 
     //const auth = Buffer.from(user + ':' + password).toString('base64');
-    const auth =  Config.auth
+    const auth = Config.auth
     const token = getCookie('USER_TOKEN');
     const username = getCookie('CONTRIBUTOR');
     //const base_domain = Config.base_domain;
 
-    const url_list = Config.api_domain + "/contribution/username/"+username;
+    const url_list = Config.api_domain + "/contribution/username/" + username;
     const url_insert_data = Config.api_domain + "/data/";
+    const url_update_data = Config.api_domain + "/data/update/";
     const url_publish_data = Config.api_domain + "/data/publish/";
     const url_insert = Config.api_domain + "/contribution/";
     const url_update = Config.api_domain + "/contribution/update/";
@@ -61,7 +62,7 @@ export default function Data(props) {
     const url_publish = Config.geoserver_domain + "rest/workspaces/fta/datastores/" + username + "/external.shp";
     //http://localhost:8600/geoserver/rest/workspaces/cifor/datastores/roads/external.shp
     //http://localhost/geoserver/rest/workspaces/cifor/datastores/emhayusa.json
-    const url_featuretypes = Config.geoserver_domain + "rest/workspaces/fta/datastores/" + fileName+"_"+username + "/featuretypes";
+    const url_featuretypes = Config.geoserver_domain + "rest/workspaces/fta/datastores/" + fileName + "_" + username + "/featuretypes";
     //http://localhost/geoserver/rest/workspaces/cifor/datastores/emhayusa/featuretypes/ADMINISTRASIDESA_AR_25K.json
     const url_wms = Config.geoserver_domain + "fta/wms?service=WMS&version=1.1.0&request=GetMap&layers=fta:"
 
@@ -152,6 +153,8 @@ export default function Data(props) {
         //console.log(wilayahId)
         if (modeUsulan === "hapus")
             return true
+        else if (modeUsulan === "ubah")
+            return name.length > 0
         else
             return name.length > 0 && selectedFile;
 
@@ -384,7 +387,7 @@ export default function Data(props) {
         //console.log(shape)
         try {
 
-            var myHeaders = new Headers(); 
+            var myHeaders = new Headers();
             myHeaders.append("Authorization", auth);
 
             var requestOptions = {
@@ -444,24 +447,24 @@ export default function Data(props) {
             const formData = new FormData();
             formData.append('file', file.files[0]);
             formData.append('username', username);
-            
+
             const requestOptions = {
                 method: 'POST',
-                headers: { 
-                    'Authorization': token 
+                headers: {
+                    'Authorization': token
                 },
                 body: formData
             };
 
-            const response = await fetch (url_insert_data, requestOptions)
-          //console.log(response)
-    
-          var json = await response.json();
-          //console.log(json);
-          //console.log(json.status);
-        
-          if (response.status === 201) {
-            //console.log(data);
+            const response = await fetch(url_insert_data, requestOptions)
+            //console.log(response)
+
+            var json = await response.json();
+            //console.log(json);
+            //console.log(json.status);
+
+            if (response.status === 201) {
+                //console.log(data);
                 //setAlert('d-block alert-success')
                 //setError('d-none')
                 //msg.innerHTML = json.message;
@@ -488,12 +491,12 @@ export default function Data(props) {
                     fetchGeoserver(event.target.width, event.target.height);
                 });
                 reflect.append(img);
-           }else{
-            setError('d-block alert-danger')  
-            err.innerHTML = `Error ${response.status} ${response.statusText}`;
-            console.error(`Error ${response.status} ${response.statusText}`);
-          }
-    
+            } else {
+                setError('d-block alert-danger')
+                err.innerHTML = `Error ${response.status} ${response.statusText}`;
+                console.error(`Error ${response.status} ${response.statusText}`);
+            }
+
 
             // Fetch data from REST API
             /*
@@ -582,7 +585,7 @@ export default function Data(props) {
             msg.innerHTML = `Error ${error}`;
             console.error(`Error ${error}`);
         }
-        
+
     };
 
     const publishData = async () => {
@@ -598,11 +601,11 @@ export default function Data(props) {
             const formData = new FormData();
             formData.append('path', path + "/" + shapeFile);
             formData.append('username', username);
-            
+
             const requestOptions = {
                 method: 'POST',
-                headers: { 
-                    'Authorization': token 
+                headers: {
+                    'Authorization': token
                 },
                 body: formData
             };
@@ -781,7 +784,7 @@ export default function Data(props) {
 
     const ubahData = async () => {
 
-        
+
         //var btn = document.querySelector("#ulangi");
         var msg = document.querySelector("#message");
         var err = document.querySelector("#error");
@@ -793,83 +796,144 @@ export default function Data(props) {
             // Fetch data from REST API
 
             var file = document.getElementById('custom-file'); //document.querySelector("#proposalFile");
+            if (file.files[0]) {
+                const formData = new FormData();
+                formData.append('file', file.files[0]);
+                formData.append('username', username);
 
-            const formData = new FormData();
-            formData.append('file', file.files[0]);
+                const requestOptions = {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': token
+                    },
+                    body: formData
+                };
 
-
-
-            var myHeaders = new Headers(); //YWRtaW46Z2Vvc2VydmVy
-            myHeaders.append("Authorization", auth);
-            myHeaders.append("Content-Type", "application/zip");
-
-
-            /*
-             const response = await fetch(url, {
-        credentials: 'include',
-        method: 'PUT',
-        headers: {
-          Authorization: 'Basic ' + auth,
-          'Content-Type': 'application/zip'
-        },
-        body: readStream
-      });
-
-            const response = await fetch(this.url + 'about/version.json', {
-                credentials: 'include',
-                method: 'GET',
-                headers: {
-                    Authorization: 'Basic ' + auth
-                }
-            });
-            */
-
-            var requestOptions = {
-                method: 'PUT',
-                headers: myHeaders,
-                body: formData,
-                redirect: 'follow'
-            };
-            /*
-            fetch("http://localhost/geoserver/rest/workspaces/cifor/datastores/emhayusa/file.shp", requestOptions)
-                .then(response => response.text())
-                .then(result => console.log(result))
-                .catch(error => console.log('error', error));
-                /
-            
-          
-          const requestOptions = {
-            method: 'POST',
-            headers: { 
-                'Authorization': token 
-            },
-            body: formData
-          };
-          */
-            const response = await fetch(url_upload, requestOptions)
-            //console.log(response)
-
-            //var rsp = await response.text();
-            //console.log(json);
-            //console.log(json.status);
-
-            if (response.status === 201) {
-                //console.log(data);
-                //setAlert('d-block alert-success')
-                //setError('d-none')
+                const response = await fetch(url_update_data, requestOptions)
                 //console.log(response)
-                //msg.innerHTML = "successfully uploaded.. wait for get wms url..";
-                //load_usulan();
-                // setId(0);
-                //setName("");
-                // setSelectedFile();
-                // setAddress("");
-                publishData()
 
+                var json = await response.json();
+                //console.log(json);
+                //console.log(json.status);
+
+                if (response.status === 201) {
+                    //console.log(data);
+                    //setAlert('d-block alert-success')
+                    //setError('d-none')
+                    //msg.innerHTML = json.message;
+                    //if(path){
+                    //    publishData()
+                    //}    
+                    //load_usulan();
+                    //setId(0);
+                    //setName("");
+                    //setFilename("");
+                    //setFormVisible(false);
+                    var reflect = document.getElementById('reflect');
+                    reflect.innerHTML = "";
+                    console.log(shapeFile);
+                    console.log(url_reflect);
+                    var img = document.createElement('img');
+                    var refl = url_reflect + shapeFile
+                    img.src = refl.replace(".shp", "");
+                    img.addEventListener('load', (event) => {
+                        console.log('image has been loaded!');
+                        console.log(event);
+                        console.log(event.target.width);
+                        console.log(event.target.height);
+                        fetchGeoserver(event.target.width, event.target.height);
+                    });
+                    reflect.append(img);
+                } else {
+                    setError('d-block alert-danger')
+                    err.innerHTML = `Error ${response.status} ${response.statusText}`;
+                    console.error(`Error ${response.status} ${response.statusText}`);
+                }
+
+
+                /*
+                const formData = new FormData();
+                formData.append('file', file.files[0]);
+
+
+
+                var myHeaders = new Headers(); //YWRtaW46Z2Vvc2VydmVy
+                myHeaders.append("Authorization", auth);
+                myHeaders.append("Content-Type", "application/zip");
+
+
+                /*
+                 const response = await fetch(url, {
+            credentials: 'include',
+            method: 'PUT',
+            headers: {
+              Authorization: 'Basic ' + auth,
+              'Content-Type': 'application/zip'
+            },
+            body: readStream
+          });
+    
+                const response = await fetch(this.url + 'about/version.json', {
+                    credentials: 'include',
+                    method: 'GET',
+                    headers: {
+                        Authorization: 'Basic ' + auth
+                    }
+                });
+                *
+
+                var requestOptions = {
+                    method: 'PUT',
+                    headers: myHeaders,
+                    body: formData,
+                    redirect: 'follow'
+                };
+                /*
+                fetch("http://localhost/geoserver/rest/workspaces/cifor/datastores/emhayusa/file.shp", requestOptions)
+                    .then(response => response.text())
+                    .then(result => console.log(result))
+                    .catch(error => console.log('error', error));
+                    /
+                
+              
+              const requestOptions = {
+                method: 'POST',
+                headers: { 
+                    'Authorization': token 
+                },
+                body: formData
+              };
+              *
+                const response = await fetch(url_upload, requestOptions)
+                //console.log(response)
+
+                //var rsp = await response.text();
+                //console.log(json);
+                //console.log(json.status);
+
+                if (response.status === 201) {
+                    //console.log(data);
+                    //setAlert('d-block alert-success')
+                    //setError('d-none')
+                    //console.log(response)
+                    //msg.innerHTML = "successfully uploaded.. wait for get wms url..";
+                    //load_usulan();
+                    // setId(0);
+                    //setName("");
+                    // setSelectedFile();
+                    // setAddress("");
+                    publishData()
+
+                } else {
+                    setError('d-block alert-danger')
+                    err.innerHTML = `Error ${response.status} ${response.statusText}`;
+                    console.error(`Error ${response.status} ${response.statusText}`);
+                }
+                */
             } else {
-                setError('d-block alert-danger')
-                err.innerHTML = `Error ${response.status} ${response.statusText}`;
-                console.error(`Error ${response.status} ${response.statusText}`);
+                //update name only
+                //alert('execute')
+                updateData(url)
             }
         } catch (error) {
             setError('d-block alert-danger')
